61aee8a18a3da2d0c8e7251431ae7dea762bf20f
JK03Coder <jnkempton@gmail.com>
2024-07-01 16:48:59 +1200

    switched to .server.ts files

---
+++ src/routes/+page.server.ts
@@ -0,0 +1,11 @@
+import getDirectusInstance from '$lib/directus';
+import type { global } from '$lib/directus';
+import { readSingleton } from '@directus/sdk';
+import type { PageServerLoad } from './$types';
+
+export const load = (async ({ fetch }) => {
+	const directus = getDirectusInstance(fetch);
+	return {
+		global: await directus.request<global>(readSingleton('global'))
+	};
+}) satisfies PageServerLoad;
---
+++ src/routes/blog/+page.server.ts
@@ -0,0 +1,24 @@
+import getDirectusInstance, { type post } from '$lib/directus';
+import { readItems, aggregate } from '@directus/sdk';
+import type { PageServerLoad } from './$types';
+
+export const load = (async ({ fetch }) => {
+	const directus = getDirectusInstance(fetch);
+
+	const total_posts = await directus.request(
+		aggregate('posts', {
+			aggregate: { count: '*' }
+		})
+	);
+
+	return {
+		total_posts: total_posts[0].count,
+		// Stream this promise to the front end so it can await over there.
+		posts: directus.request<post[]>(
+			readItems('posts', {
+				fields: ['slug', 'title', 'publish_date', 'description'],
+				sort: ['-publish_date']
+			})
+		)
+	};
+}) satisfies PageServerLoad;
